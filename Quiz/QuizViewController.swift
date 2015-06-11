91356de4e0694731516cefbd26624385f222f2cf
Takatoshi Watanabe <abacus.taka-1125@i.softbank.jp>
2015-06-11 17:56:13 +0900

    Quiz

--- Quiz/QuizViewController.swift
+++ Quiz/QuizViewController.swift
@@ -9,75 +9,152 @@
 import UIKit
 
 class QuizViewController: UIViewController {
+    
+    //時間計測用の変数.
+    var cnt : Float = 5
+    
+    //時間表示用のラベル.
+    var myLabel : UILabel!
+    
+    
+    //var number: Int = 0
+    
+    //@IBOutlet var label: UILabel!
+
+    
+    
     //出題数
     var questionNumber:Int = 5
-    //何問正解したかの共有
-    var correntAnswer:Int = 0
-    //randomの共有
-    var random:Int = 0
-    //解いた問題数の共有
+    
+    //現在の問題数
     var sum:Int = 0
-    //Quiz配列
-    var quiz=[NSArray]()
-    //weak参照
-    @IBOutlet weak var QuizText: UITextView!
+    
+    //正解数
+    var correctAnswer:Int = 0
+    
+    //乱数
+    var random:Int = 0
+    
+    //クイズを格納する配列
+    var quizArray = [NSMutableArray]()
+    
+    //クイズを表示するTextView
+    @IBOutlet var quizTextView: UITextView!
+    
+    //選択肢のボタン
+    @IBOutlet var choiceButtons: Array<UIButton>!
+    
+    @IBOutlet var Button: UIButton!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //------------------------ここから下にクイズを書く------------------------//
-        quiz.append(["問題文","選択肢","選択肢2","選択肢3",1])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        
-        //------------------------ここから下にクイズを書く------------------------//
+        quizArray.append(["英語の頭文字を略した言葉「PR」、「P」は「Public」、では「R」何の略？","Records","Relations","Researches",2])
+        quizArray.append(["パソコン周辺機器をつなぐために用いられる「USB」の正式名称は？","ユニバーサル・シリアル・バス","ユニバーサル・ソフト・ボード","ユニバーサル・シリコン・バス",1])
+        quizArray.append(["「ゼネコン」は何の略？","ゼネラル・コンフィデンシャル","ゼネラル・コントラクター","ゼネラル・コントロール",2])
+        quizArray.append(["英語で「yahoo」の意味は？","賢者","怠け者","田舎者",3])
+        quizArray.append(["ハーブの「ローズマリー」はラテン語でどんな意味？","海のしずく","山の喜び","大地のリンゴ",1])
+        quizArray.append(["英語の「japan」の意味は？","和紙","漆","筆",2])
+        quizArray.append(["経済成長が著しい4ヵ国の総称「BRICs」の「I」は？","インドネシア","イタリア","インド",3])
+        quizArray.append(["「クローン」の本来の意味は何でしょう？","真似","小枝","集合体",2])
+        quizArray.append(["イタリア語で「ミネストローネ」の意味は？","具沢山","元気","健康",1])
+        quizArray.append(["JAXA（ジャクサ）の正式名称は何でしょう？","宇宙航空研究開発機構","宇宙開発研究機構","日本航空宇宙局",1])
 
-        random = Int(arc4random()%UInt32(quiz.count))
-        QuizText.text=quiz[random][0] as NSString
+        //------------------------ここから下にクイズを書く------------------------//
+        choiceQuiz()
+        
+        
+        //ラベルを作る.
+        myLabel = UILabel(frame: CGRectMake(0,0,200,50))
+        myLabel.backgroundColor = UIColor.blueColor()
+        myLabel.layer.masksToBounds = true
+        myLabel.layer.cornerRadius = 20.0
+        myLabel.text = "Time:\(cnt)"
+        myLabel.textColor = UIColor.whiteColor()
+        myLabel.shadowColor = UIColor.grayColor()
+        myLabel.textAlignment = NSTextAlignment.Center
+        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 200)
+        self.view.backgroundColor = UIColor.cyanColor()
+        self.view.addSubview(myLabel)
+        
+        //タイマーを作る.
+        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "onUpdate:", userInfo: nil, repeats: true)
+        
+
+    }
+    
+    func choiceQuiz() {
+        println(quizArray.count)
+        //クイズの問題文をシャッフルしてTextViewにセット
+        random = Int(arc4random_uniform(UInt32(quizArray.count)))
+        quizTextView.text = quizArray[random][0] as! NSString as String
+        
+        //選択肢のボタンにそれぞれ選択肢のテキストをセット
+        for var i = 0; i < choiceButtons.count; i++ {
+            choiceButtons[i].setTitle(quizArray[random][i+1] as! NSString as String, forState: .Normal)
+            
+            //どのボタンが押されたか判別するためのtagをセット
+            choiceButtons[i].tag = i + 1;
+        }
+    }
+
+    
+    @IBAction func choiceAnswer(sender: UIButton) {
+        sum++
+        println("random \(random)")
+        if quizArray[random][4] as! Int == sender.tag {
+            //正解数を増やす
+            correctAnswer++
+        }
+        
+        //解いた問題数の合計が予め設定していた問題数に達したら結果画面へ
+        if sum == questionNumber {
+            performSegueToResult()
+        }
+        quizArray.removeAtIndex(random)
+        choiceQuiz()
         
     }
-    @IBAction func Select1() {
-        sum++
-        if 1==quiz[random][4] as NSObject{
-            current()
+    
+    //NSTimerIntervalで指定された秒数毎に呼び出されるメソッド.
+    func onUpdate(timer : NSTimer){
+        
+        cnt -= 0.1
+        if cnt < 0{
+            cnt = 0
+            performSegueToResult()
         }
-        if(sum == questionNumber){
-            tapBtn()
-        }
+        
+        //桁数を指定して文字列を作る.
+        let str = "Time:".stringByAppendingFormat("%.1f",cnt)
+        
+        myLabel.text = str
+        
     }
-    @IBAction func Select2() {
-        sum++
-        if 2==quiz[random][4] as NSObject {
-            current()
-        }
-        if sum == questionNumber{
-            tapBtn()
-        }
-    }
-    @IBAction func Select3() {
-        sum++
-        if 3==quiz[random][4] as NSObject {
-            current()
-        }
-        if sum == questionNumber{
-            tapBtn()
-        }
-    }
-    func current(){
-        println("true")
-        correntAnswer++
-    }
-    func tapBtn() {
+    
+
+    
+    func performSegueToResult() {
         performSegueWithIdentifier("toResultView", sender: nil)
     }
-/*
+    
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "toResultView") {
+            
+            var ResultView : ResultViewController = segue.destinationViewController as! ResultViewController
+
+            ResultView.correctAnswer = self.correctAnswer
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-*/
+    
 }
+
+
